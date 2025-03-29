db8663820cdc7a5337679c8d84e7a1d859bc6286
mj99k14 <ekfkawnl644@gmail.com>
2025-03-29 16:41:47 +0900

    0329

--- javaScript/test.js
+++ javaScript/test.js
@@ -1,25 +1,33 @@
 const readline = require('readline');
 
-const r1 = readline.createInterface({
+const q = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-r1.question("첫 번째 숫자를 입력하세요: ", (answer1) => {
-    r1.question("두 번째 숫자를 입력하세요: ", (answer2) => {
-        const x = Number(answer1);
-        const y = Number(answer2);
+q.question = ("첫번쨰 입력을 하세요", (one) => {
+    q.question("두번쨰 입력을 하세요", (two) => {
+        const a = Number(one);
+        const b = Number(two);
 
-        const plus = (x, y) => x + y;
-        const divide = (x, y) => x / y;
-        const multiply = (x, y) => x * y;
-        const subtract = (x, y) => x - y;
+        const p = (a, b) => a + b;
+        const m = (a, b) => a - b;
+        const s = (a, b) => a * b;
+        const n = (a, b) => a / b;
 
-        console.log("덧셈 결과:", plus(x, y));
-        console.log("뺄셈 결과:", subtract(x, y));
-        console.log("곱셈 결과:", multiply(x, y));
-        console.log("나눗셈 결과:", divide(x, y));
 
-        r1.close();
+        setTimeout(() => {
+            console.log(p(a, b))
+            console.log(m(a, b))
+            console.log(s(a, b))
+            console.log(n(a, b))
+
+            q.close();
+        }, 3000);
     });
+
 });
+
+
+
+
