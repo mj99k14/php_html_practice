1cd283cfe807f05894429476c162b5452dedc2cb
mj99k14 <ekfkawnl644@gmail.com>
2025-03-30 15:44:09 +0900

    javascript연습

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,9 +1,35 @@
-const heropy = {
-  firstName: 'Heropy',
-  lastName: 'Park',
-  getFulName: function () {
+/*function user(first, last) {
+  this.firstName = first
+  this.lastName = last
+
+}
+user.prototype.getFulName = function () {
+  return `${this.firstName} ${this.lastName}`
+}
+const heropy = new user('Heropy', 'Park')
+const amy = new user('Amy', 'Clarke')
+const neo = new user('Neo', 'Smith')
+
+console.log(heropy.getFulName())
+console.log(amy.getFulName())
+console.log(neo.getFulName())
+
+*/
+//ES6
+class User {
+  constructor(first, last) {
+    this.firstName = first
+    this.lastName = last
+  }
+  getFulName() {
     return `${this.firstName} ${this.lastName}`
   }
 }
 
+const heropy = new User('Heropy', 'Park')
+const amy = new User('Amy', 'Clarke')
+const neo = new User('Neo', 'Smith')
+
 console.log(heropy.getFulName())
+console.log(amy.getFulName())
+console.log(neo.getFulName())
